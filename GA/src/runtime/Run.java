e918516af64e86569a4fefae93e2e8ff3789e9ab
dan <9dgonzalezg@fraternidad.com>
2013-07-16 15:51:53 +0200

    Actualizacion memoria

--- GA/src/runtime/Run.java
+++ GA/src/runtime/Run.java
@@ -36,44 +36,19 @@ public class Run {
 		*/
 		String[] command ={"cmd"};
 		String[] envp = {"path="+System.getenv("Path")};		
-	    Process p = Runtime.getRuntime().exec(command, envp);
+	    Process p = Runtime.getRuntime().exec(command, envp);	    
 	    new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
 	    new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
-	    PrintWriter stdin = new PrintWriter(p.getOutputStream());
-	    String[] ind={"EPSILON","IGD","SPREAD","HV"};
+	    PrintWriter stdin = new PrintWriter(p.getOutputStream());	    
 	    stdin.println("cd C:\\Users\\9dgonzalezg\\Desktop\\workspace\\GA\\" +
-	    		"GA\\resultsMyNSGAIIExperiment\\data\\");
-	    for (int i = 0; i < ind.length; i++) {
-	    	new Run().touchContrastar(ind[i]);
-		    stdin.println("rscript contrastar.r");
-		    
-		}	    
+	    		"GA\\resultsMyNSGAIIExperiment\\data\\");	    
+	    stdin.println("rscript contrastarEPSILON.r");	    
+	    stdin.println("rscript contrastarIGD.r");		    
+	    stdin.println("rscript contrastarSPREAD.r");	    
+	    stdin.println("rscript contrastarHV.r");	    
 	    stdin.close();
 	    int returnCode = p.waitFor();
 	    System.out.println("Return code = " + returnCode);
 	}
-	
-	private void touchContrastar(String s) throws IOException{
-		BufferedReader br=new BufferedReader(new FileReader(
-				"C:\\Users\\9dgonzalezg\\Desktop\\workspace\\GA\\" +
-	    		"GA\\resultsMyNSGAIIExperiment\\data\\contrastar.r"));
-		PrintWriter pw=new PrintWriter("C:\\Users\\9dgonzalezg\\Desktop\\workspace\\GA\\" +
-	    		"GA\\resultsMyNSGAIIExperiment\\data\\contrastarAux.r");
-		String str=br.readLine();
-		while(str!=null){
-			if(str.contains("fuentesdatos<-list.files(pattern=")){
-				pw.println("fuentesdatos<-list.files(pattern=\""+s+".res\")");
-			}else pw.println(str);
-			str=br.readLine();
-		}
-		br.close();
-		pw.close();
-		new File("C:\\Users\\9dgonzalezg\\Desktop\\workspace\\GA\\" +
-	    		"GA\\resultsMyNSGAIIExperiment\\data\\contrastar.r").delete();
-		new File("C:\\Users\\9dgonzalezg\\Desktop\\workspace\\GA\\" +
-	    		"GA\\resultsMyNSGAIIExperiment\\data\\contrastarAux.r").renameTo(
-	    				new File("C:\\Users\\9dgonzalezg\\Desktop\\workspace\\GA\\" +
-	    			    		"GA\\resultsMyNSGAIIExperiment\\data\\contrastar.r"));
-	}
-
+		
 }
